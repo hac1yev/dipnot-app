eef6c009a64524f3277a541cd58852e22e05e1d8
hac1yev <ilkinhaciyev955@gmail.com>
2024-07-02 22:29:16 +0400

    menu

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import { styled, createTheme, ThemeProvider, alpha } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import SearchIcon from '@mui/icons-material/Search';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 // import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
@@ -18,6 +20,8 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import dipnot_logo from '../../assets/dipnote-logo.svg';
+import { Avatar, InputBase, Stack, useMediaQuery } from '@mui/material';
+import './Dashboard.scss'
 // import { mainListItems, secondaryListItems } from './listItems';
 // import Chart from './Chart';
 // import Deposits from './Deposits';
@@ -41,6 +45,44 @@ const AppBar = styled(MuiAppBar, {
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
+}));
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  border: '1px solid #ccc',
+  borderRadius: '20px',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '90%',
+  [theme.breakpoints.up('md')]: {
+    marginLeft: theme.spacing(3),
+    width: '70%',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
 }));
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
@@ -74,6 +116,8 @@ const defaultTheme = createTheme();
 
 export default function Dashboard() {
   const [open, setOpen] = React.useState(true);
+  const matches = useMediaQuery('(min-width:600px)');
+
   const toggleDrawer = () => {
     setOpen(!open);
   };
@@ -82,47 +126,75 @@ export default function Dashboard() {
     <ThemeProvider theme={defaultTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="absolute" open={open} >
+        <AppBar 
+          position="absolute" 
+          open={open} 
+          sx={{ 
+            bgcolor: '#fff',
+            boxShadow: `
+              0px 2px 4px -1px rgba(0,0,0,0.04),
+              0px 4px 5px 0px rgba(0,0,0,0.04),
+              0px 1px 10px 0px rgba(0,0,0,0.04)
+            `,
+            height: '90px',
+            justifyContent: 'center',
+          }}
+        >
           <Toolbar
             sx={{
-              pr: '24px', // keep right padding when drawer closed
+              p: 0, // keep right padding when drawer closed
             }}
           >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
             <Typography
               component="h1"
               variant="h6"
               color="#000"
+              
               noWrap
-              sx={{ flexGrow: 1 }}
+              sx={{ flexGrow: 1, display: 'flex', px: 3, alignItems: 'center' }}
             >
-              Dashboard
+              <IconButton
+                edge="start"
+                color="#000"
+                aria-label="open drawer"
+                onClick={toggleDrawer}
+                sx={{
+                  marginRight: '10px',
+                  // marginLeft: '20px'
+                }}
+              >
+                <MenuIcon sx={{ fontSize: '30px' }} />
+              </IconButton>
+              <Search sx={{ height: '100%' }}>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Axtar…"
+                  sx={{ width: '100%' }}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
             </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
+            <Stack direction="row" spacing={matches ? 2 : 1}>
+              <IconButton color="#000" sx={{ bgcolor: 'rgba(231, 231, 231, 1)' }}>
+                <AddCircleOutlineIcon />
+              </IconButton>
+              <IconButton color="#000" sx={{ bgcolor: 'rgba(231, 231, 231, 1)' }}>
+                <NotificationsNoneIcon />
+              </IconButton>
+              <Avatar alt="Remy Sharp" src="" />
+            </Stack>
           </Toolbar>
         </AppBar>
-        <Drawer variant="permanent" open={open}>
+        <Drawer variant="permanent" open={open} sx={{ height: '100px' }}>
           <Toolbar
             sx={{
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
               px: [1],
+              height: '90px'
             }}
           >
             <Box 
@@ -134,16 +206,6 @@ export default function Dashboard() {
               alt="The house from the offer."
               src={dipnot_logo}
             />
-            <IconButton
-              sx={{
-                position: 'absolute',
-                right: '0px',
-                
-              }} 
-              onClick={toggleDrawer}
-            >
-              <ChevronLeftIcon />
-            </IconButton>
           </Toolbar>
           <Divider />
           {/* <List component="nav">
@@ -165,7 +227,7 @@ export default function Dashboard() {
           }}
         >
           <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          {/* <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
               <Grid item xs={12} md={7} lg={8}>
                 <Paper
@@ -187,11 +249,10 @@ export default function Dashboard() {
                     height: 240,
                   }}
                 >
-                  {/* <Deposits /> */}
                 </Paper>
               </Grid>
             </Grid>
-          </Container>
+          </Container> */}
         </Box>
       </Box>
     </ThemeProvider>
