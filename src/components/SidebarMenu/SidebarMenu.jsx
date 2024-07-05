7513d057a57f509db7f953630d9561c3aa62370d
hac1yev <ilkinhaciyev955@gmail.com>
2024-07-05 16:13:56 +0400

    sidebar menu

--- src/components/SidebarMenu/SidebarMenu.jsx
+++ src/components/SidebarMenu/SidebarMenu.jsx
@@ -1,20 +1,12 @@
 import { Link, useLocation } from "react-router-dom";
-import {
-  Avatar,
-  Box,
-  Divider,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
+import { Avatar, Box, Button, Divider, List, ListItem, ListItemAvatar, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import StarIcon from "@mui/icons-material/Star";
 import ImageIcon from "@mui/icons-material/Image";
 import WorkIcon from "@mui/icons-material/Work";
 import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import BuildOutlinedIcon from '@mui/icons-material/BuildOutlined';
 import "./SidebarMenu.scss";
 
 const SidebarMenu = () => {
@@ -22,6 +14,7 @@ const SidebarMenu = () => {
 
   return (
     <Box className="sidebar-wrapper">
+
       <List sx={{ pb: '10px' }}>
         <Link to="/">
           <ListItem
@@ -58,88 +51,106 @@ const SidebarMenu = () => {
           </ListItem>
         </Link>
       </List>
+
       <Divider />
-      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        <ListItem sx={{ px: 0 }}>
+
+      <Box sx={{ pt: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Typography sx={{ color: 'rgba(2, 66, 137, 1)', fontSize: '20px', fontWeight: '500' }}>Qruplar</Typography>
+        <Box 
+          component={"span"} 
+          sx={{ 
+            borderRadius: '14.59px', 
+            bgcolor: 'rgba(0, 32, 67, 1)', 
+            padding: '1px 12px', 
+            color: '#fff' 
+          }}
+        >
+          19
+        </Box>
+      </Box>
+
+      <List className="sidebar-menu-groups" sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <ListItem>
           <ListItemAvatar>
             <Avatar>
               <ImageIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+          <ListItemText primary="Website Design" secondary="679 üzv" />
         </ListItem>
-        <ListItem sx={{ px: 0 }}>
+        <ListItem>
           <ListItemAvatar>
             <Avatar>
               <WorkIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText primary="Work" secondary="Jan 7, 2014" />
+          <ListItemText primary="Frontend Development" secondary="537 üzv" />
         </ListItem>
-        <ListItem sx={{ px: 0 }}>
+        <ListItem>
           <ListItemAvatar>
             <Avatar>
               <BeachAccessIcon />
             </Avatar>
           </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
+          <ListItemText primary="Software Development" secondary="324 üzv" />
         </ListItem>
-        <ListItem sx={{ px: 0 }}>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
-        <ListItem sx={{ px: 0 }}>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
-        <ListItem sx={{ px: 0 }}>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
-        <ListItem sx={{ px: 0 }}>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
-        <ListItem sx={{ px: 0 }}>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
-        <ListItem sx={{ px: 0 }}>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
-        <ListItem sx={{ px: 0 }}>
-          <ListItemAvatar>
-            <Avatar>
-              <BeachAccessIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Vacation" secondary="July 20, 2014" />
-        </ListItem>
+      </List>
+
+      <Link to="/">
+        <Button variant="outlined">Hamısına bax</Button>
+      </Link>
+
+      <Divider sx={{ py: 1 }} />
+
+      <Box sx={{ pt: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Typography sx={{ color: 'rgba(2, 66, 137, 1)', fontSize: '20px', fontWeight: '500' }}>Kategoriyalar</Typography>
+      </Box>
+
+      <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: '12px' }} className="sidebar-menu-category">
+        <Link to="/">
+          <Typography sx={{ color: 'rgba(51, 51, 51, 1)', fontSize: '18px', fontWeight: '500' }}>Dizayn</Typography>
+        </Link>
+        <Link to="/">
+          <Typography sx={{ color: 'rgba(51, 51, 51, 1)', fontSize: '18px', fontWeight: '500' }}>Texnologiya</Typography>
+        </Link>
+        <Link to="/">
+          <Typography sx={{ color: 'rgba(51, 51, 51, 1)', fontSize: '18px', fontWeight: '500' }}>Proqramlaşdırma</Typography>
+        </Link>
+      </Box>
+      
+      <Link to="/">
+        <Button variant="outlined">Hamısına bax</Button>
+      </Link>
+
+      <Divider sx={{ py: 1 }} />
+
+      <List sx={{ pb: '10px' }}>
+        <Link to="/">
+          <ListItem
+            disablePadding
+            className="sidebar-list-item"
+          >
+            <ListItemButton>
+              <ListItemIcon>
+                <HelpOutlineOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="Kömək" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link to="/">
+          <ListItem
+            disablePadding
+            className="sidebar-list-item"
+          >
+            <ListItemButton>
+              <ListItemIcon>
+                <BuildOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText primary="App & Tools" />
+            </ListItemButton>
+          </ListItem>
+        </Link>
       </List>
     </Box>
   );
