595f77d6da2a5418f0eec4045a731a20c5f7d4e0
hac1yev <ilkinhaciyev955@gmail.com>
2024-07-08 13:21:56 +0400

    images

--- src/dummy-data/communitydata.js
+++ src/dummy-data/communitydata.js
@@ -2,34 +2,34 @@ export const community_data = [
     [
         {
             id: 't1',
-            img: '/public/community/c1.svg',
+            img: '/community/c1.svg',
             title: 'Texnologiya'
         },
         {
             id: 't2',
-            img: '/public/community/c2.svg',
+            img: '/community/c2.svg',
             title: 'Dizayn'
         },
         {
             id: 't3',
-            img: '/public/community/c3.svg',
+            img: '/community/c3.svg',
             title: 'Proqramlaşdırma'
         },
     ],
     [
         {
             id: 't4',
-            img: '/public/community/c4.svg',
+            img: '/community/c4.svg',
             title: 'Texnologiya'
         },
         {
             id: 't5',
-            img: '/public/community/c5.svg',
+            img: '/community/c5.svg',
             title: 'Dizayn'
         },
         {
             id: 't6',
-            img: '/public/community/c6.svg',
+            img: '/community/c6.svg',
             title: 'Proqramlaşdırma'
         },
     ]
