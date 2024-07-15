32c5bc0c172d7333491988f0cc3556ca367b21dd
hac1yev <ilkinhaciyev955@gmail.com>
2024-07-15 17:32:12 +0400

    profile page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Toolbar } from '@mui/material';
+import { Box, Grid, Toolbar } from '@mui/material';
 import Community from '../../components/Home/Community';
 import Posts from '../../components/Home/Posts';
 import './Home.scss';
